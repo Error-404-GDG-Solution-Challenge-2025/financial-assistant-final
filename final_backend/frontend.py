f98777aee559705432ad2273a9b70d2095860392
painful-bug <ujanaishik109@gmail.com>
2025-03-27 03:29:57 +0530

    forced streamlit cloud to reinstall sqlite3

--- final_backend/frontend.py
+++ final_backend/frontend.py
@@ -1,4 +1,7 @@
 # frontend.py
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 # from langchain.agents import ConversationalChatAgent, AgentExecutor # Not used directly now
